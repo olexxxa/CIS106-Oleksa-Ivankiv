40fcbc9224e00d03b95fa1a9dbf03b19fa8be36c
olexxxa <olexxxa@gmail.com>
2021-02-28 12:27:54 -0600

    Add files via upload
    Session 2 assignment

---
+++ Assignment 1.4.py
@@ -0,0 +1,3 @@
+print("Please enter amount for a meal")
+a = float(input())
+print("Your total tip amount is " + str(a * 0.15))
---
+++ Assignment 1.5.py
@@ -0,0 +1,3 @@
+print("Please enter amount of item purchuased")
+a = float(input())
+print("Your amount is " + str(a) + " And your 7% tax amount is " + str(a * 0.07) + " Then your grand total amount is " + str((a * 0.07 + a)))
