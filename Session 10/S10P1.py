9e3e4e79018436b6007178966ad9657f2054b33e
olexxxa <olexxxa@gmail.com>
2021-05-09 13:56:31 -0500

    Create S10P1.py

---
+++ Session 10/S10P1.py
@@ -0,0 +1,12 @@
+def printarray(arr):
+  print ("List of last names:", arr)
+
+def printreverse(arr):
+  arr.reverse()
+  print ("Reversed list of names:", arr)
+
+
+arr = ['Ivankiv','Todorov','Hrab','Kotil']
+
+printarray(arr)
+printreverse(arr)
